4b2337b96aef526ac4182c13719b12a2ebcb264d
proloyroy198817-cyber2 <proloyroy198817@gmail.com>
2026-07-27 21:13:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let score = 0;
 
+const balloon = document.getElementById("balloon");
+const scoreText = document.getElementById("score");
+
+function moveBalloon() {
+    const maxX = window.innerWidth - 100;
+    const maxY = window.innerHeight - 200;
+
+    const x = Math.floor(Math.random() * maxX);
+    const y = Math.floor(Math.random() * maxY);
+
+    balloon.style.left = x + "px";
+    balloon.style.top = y + "px";
+}
+
+balloon.addEventListener("click", function () {
+    score++;
+    scoreText.innerText = score;
+    moveBalloon();
+});
+
+moveBalloon();
